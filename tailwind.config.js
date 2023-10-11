7e054e6ae740e1f28678a62aafdda4527c6244f6
zikran69 <zikzigi69@gmail.com>
2023-10-12 02:58:12 +0700

    start build portofolio project with react

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      primary: "Montserrat",
+      secondary: "Orbitron",
+    },
+    extend: {
+      colors: {
+        primary: "#B809C3",
+        secondary: "#ff014f",
+        third: "#FEF6C7",
+        fourth: "#011eff",
+      },
+    },
+  },
+  plugins: [],
+};
