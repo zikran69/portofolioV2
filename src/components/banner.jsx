083d166380ed69dd653b7a0db98e97386ce490f8
zikran69 <zikzigi69@gmail.com>
2023-10-12 09:23:34 +0700

    adding banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-
+import { TypeAnimation } from "react-type-animation";
 export default function Banner() {
   return (
-    <div id="home" className="section">
-      Banner
+    <div
+      id="home"
+      className="section container mx-auto mas-w-[1200px] px-3 items-center">
+      <div className="w-full">
+        <div className="block text-center font-secondary font-black text-[30px]">
+          Hello, I'm
+          <div>
+            <TypeAnimation
+              sequence={[
+                "Zikran",
+                3000,
+                "Full Stack Developer",
+                2000,
+                "Front-end Web Developer",
+                2000,
+                "Back-end Web Developer",
+                2000,
+              ]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+              className="ml-3 text-secondary"
+            />
+          </div>
+          <h4 className="font-primary font-normal">
+            Welcome to my website, here you can see the information you need
+            about me, I hope this website can help you and me in achieving the
+            goals we hope for.
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
