6a0a03c9a80d78dc37dabe384c870598273a3e7a
zikran69 <zikzigi69@gmail.com>
2023-10-31 15:21:42 +0700

    add footer file

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="container mx-auto justify-center items-center text-center mt-[10px] mb-[20px]">
+      copyright : Asrari ZIkran 2023
+    </div>
+  );
+}
